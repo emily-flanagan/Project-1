52e67bcb073cd36f7a04211e490467d60b87e198
Emily Flanagan <emilyflanagan@Emilys-MacBook-Air.local>
2026-02-23 20:48:51 -0500

    attempt 1

--- app/src/main/java/com/example/project1/Project1.kt
+++ app/src/main/java/com/example/project1/Project1.kt
@@ -1,13 +1,14 @@
 package com.example.counterapp
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,6 +24,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun LoginScreen(modifier: Modifier=Modifier) {
@@ -62,6 +64,57 @@ fun LoginScreen(modifier: Modifier=Modifier) {
 
 fun checkUsernameAndPassword(username: String, password: String): Boolean{
     return username.isNotBlank()&&password.isNotBlank()
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier=Modifier) {
+    var search by remember{ mutableStateOf("") }
+    val context = LocalContext.current
+    Column(
+        modifier=modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        TextField(value=search,
+            onValueChange = {newValue ->
+                search=newValue
+            },
+            label={Text("Search Term")},
+            modifier=Modifier.padding(8.dp))
+        Button (onClick = {
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+        },
+            enabled = checkSearch(search)){
+            Text("Search")
+        }
+    }
+
+    Row(
+        modifier=modifier.fillMaxSize()
+
+    ) {
+        Spacer(Modifier.width(50.dp))
+        Button(
+            onClick = {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            }
+        ) {
+            Text("Local News")
+        }
+        Spacer(Modifier.width(50.dp))
+        Button(
+            onClick = {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            }
+        ) {
+            Text("Top Headlines")
+        }
+        Spacer(Modifier.width(50.dp))
+    }
+}
+
+fun checkSearch(search: String): Boolean{
+    return search.isNotBlank()
 }
 
 
